ca2745f357e2aaa456a06b9b68e8631e58a37ff8
jit0341 <jitendrablog6@gmail.com>
2025-05-24 16:51:39 +0530

    Create age_group.py

---
+++ age_group.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+            if age < 0 or age > 120:
+    print("Invalid age")
+elif age <= 12:
+    print("Child")
+elif age <= 19:
+    print("Teenager")
+elif age <= 59:
+    print("Adult")
+else:
+    print("Senior")
