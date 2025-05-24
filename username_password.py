f5711f692b5e095cfe4b000099c2f5b38185ce2e
jit0341 <jitendrablog6@gmail.com>
2025-05-24 17:21:34 +0530

    Create username_password.py

---
+++ username_password.py
@@ -0,0 +1,8 @@
+correct_username = "admin"
+correct_password = 1234
+username = str(input(" Enter username:"))
+password = int(input("Enter Password: "))
+if username == correct_username and password == correc_password:
+    print("Login successful ")
+else:
+    print("Access Denied ")
