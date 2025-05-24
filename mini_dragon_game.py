4fe1877db061e807f51b436192763733de6ccc53
jit0341 <jitendrablog6@gmail.com>
2025-05-24 17:07:19 +0530

    Create mini_dragon_game.py

---
+++ mini_dragon_game.py
@@ -0,0 +1,17 @@
+import random
+
+# Give your dragon a name
+your_name = input("Name your dragon: ")
+your_power = random.randint(50, 100)
+
+# Enemy dragon
+enemy_name = "Shadowfang"
+enemy_power = random.randint(50, 100)
+
+# Show stats
+print("\nBattle Begins!")
+print(your_name, "Power:", your_power)
+print(enemy_name, "Power:", enemy_power)
+
+# Decide winner
+if your_power > enemy_power:
