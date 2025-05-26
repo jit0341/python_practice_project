411803e04ef574641ebc7ced46f9ea1ef8ed458d
jit0341 <jitendrablog6@gmail.com>
2025-05-26 06:38:14 +0530

    Update if_else_quiz.py

--- if_else_quiz.py
+++ if_else_quiz.py
@@ -1,4 +1,6 @@
-# Description: This script checks if a user is eligible to vote based on age input.
+# Description #
+# This script checks if a user is
+eligible to vote based on age input.
 age = int(input("Enter your age: "))
 
 if age >= 18:
