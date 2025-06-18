b3c9663170252b13003b7f95d95aaeb87de5578c
jit0341 <jitendrablog6@gmail.com>
2025-06-18 07:00:41 +0530

    First file deals with function using args (dicstrings) and 5 questions secind file starts with function basics

---
+++ functions.py
@@ -0,0 +1,11 @@
+def multiply(a,b):
+     return  a * b
+
+print(multiply(5,3))
+
+def greet_person(name, time="morning"):
+    print(f"Good {time}, {name}!")
+
+# Test it
+greet_person("Jitendra")           # Output: Good morning, Jitendra!
+greet_person("Keshav", "evening")  # Output: Good evening, Keshav!
