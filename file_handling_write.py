c528319eef19e1124027599fa2fd6bb798b6b8e8
jit0341 <jitendrablog6@gmail.com>
2025-06-29 19:50:11 +0530

    Added the modifications.

--- file_handling_write.py
+++ file_handling_write.py
@@ -1,5 +1,8 @@
-# Step 1: Open a file in write mode
-file = open("my_notes.txt", "w")
+# file_handling_write.py
+
+# Step 1: Open a file in write mode using the FULL path to your project folder
+# This ensures my_notes.txt is created/updated inside your python_practice_project folder
+file = open("/storage/emulated/0/python_practice_project/my_notes.txt", "w")
 
 # Step 2: Write some text
 file.write("This is my first file in Python!\n")
@@ -8,13 +11,13 @@ file.write("Learning file handling is fun.")
 # Step 3: Close the file
 file.close()
 
+# The 'os' module allows interaction with the operating system
 import os
+# This prints Pydroid's default current working directory, useful for debugging
+print("Current working directory (Pydroid's default):", os.getcwd())
 
-print("Current working directory:")
-print(os.getcwd())
-
-
-with open("/storage/emulated/0/my_notes.txt", "r") as file:
+# To read the file you just wrote, use the FULL path to your project folder
+print("\nReading file content from the project folder:")
+with open("/storage/emulated/0/python_practice_project/my_notes.txt", "r") as file:
     content = file.read()
-    print("File content:\n")
     print(content)
